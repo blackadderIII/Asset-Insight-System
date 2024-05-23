94a1e6b11b905cdb0e0db383f416600f141d510b
blackadderIII <skobarnes1@gmail.com>
2024-05-23 18:56:21 +0000

    C5

---
+++ Asset Insight System/a.i.s/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './Components/all.css'
+import Navbar from './Components/Navbar.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
