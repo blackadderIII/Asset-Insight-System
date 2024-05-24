49cf9e9b0ac1a2fbe341e17c03c5a6c5ae2930eb
blackadderIII <skobarnes1@gmail.com>
2024-05-24 09:05:09 +0000

    started to create pages

---
+++ Asset Insight System/a.i.s/frontend/src/Pages/Asset2.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import '../css/Table.css'
+
+function Asset1() {
+  return (
+    <section class="main">
+        <div class="first-row">
+            <div class="greeting">
+                <h1>Phones</h1>
+                <p>An overview of the laptops in the origanization</p>
+            </div>
+            <div class="date">
+                <span id="todaysDate">Today, November 22, 2023</span>
+            </div>
+        </div>
+
+        <div class="second-row">
+            <div class="buttons">
+                <div class="add-button" onclick="openModule()">
+                    <i class="fal fa-add"></i>
+                    <h5>Add</h5>
+                </div>
+                <div class="add-button" onclick="exportLaptops()">
+                    <i class="fal fa-file-export"></i>
+                    <h5>Export</h5>
+                </div>
+            </div>
+            <div class="field">
+                <input type="search" id="searchAsset" placeholder="Search..."/>
+                <i class="fal fa-search"></i>
+            </div>
+        </div>
+
+        <div class="third-row">
+            <table id="laptops">
+                <thead>
+                    <th>SN</th>
+                    <th>Brand</th>
+                    <th>Model</th>
+                    <th></th>
+                    <th>User</th>
+                    <th>Date Added</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </thead>
+
+                <tbody id="main-table">
+
+                    <div class="loading" id="loading"></div>
+
+                </tbody>
+
+                <tbody class="inactive" id="search-table">
+                </tbody>
+            </table>
+        </div>
+
+    </section>
+  )
+}
+
+export default Asset1
---
+++ Asset Insight System/a.i.s/frontend/src/Pages/Dashboard.jsx
@@ -0,0 +1,306 @@
+import React from "react";
+import "../css/dashboard.css";
+
+function Dashboard() {
+  
+
+  return (
+      <section className="mainDash">
+        <div className="first-row">
+          <div className="greeting">
+            <h1>
+              Hello <span id="greeting-userName"></span> 👋🏽
+            </h1>
+            <p>An overview of all the assets in the organization</p>
+          </div>
+          <div className="date">
+            <span id="todaysDate">Today, January 01, 2024</span>
+          </div>
+        </div>
+
+        <div className="second-row">
+          <div className="singleStats">
+            <div className="single-stat" id="laptops">
+              <div className="singleStat-total">
+                <i className="fal fa-laptop"></i>
+                <h4>Laptops</h4>
+                <h1 id="laptops-total">
+                  <div className="loading-mini"></div>
+                </h1>
+              </div>
+              <div className="single-stat-extra">
+                <div className="extra-stat">
+                  <div className="indicator a"></div>
+                  <h5>Assigned</h5>
+                  <span id="assignedL">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator u"></div>
+                  <h5>Usused</h5>
+                  <span id="unusedL">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator r"></div>
+                  <h5>Retired</h5>
+                  <span id="retiredL">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator d"></div>
+                  <h5>Damaged</h5>
+                  <span id="damagedL">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator o"></div>
+                  <h5>Out of Sevice</h5>
+                  <span id="oosL">0</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="single-stat" id="phones">
+              <div className="singleStat-total">
+                <i className="fal fa-mobile-screen"></i>
+                <h4>Phones</h4>
+                <h1 id="phones-total">
+                  <div className="loading-mini"></div>
+                </h1>
+              </div>
+              <div className="single-stat-extra">
+                <div className="extra-stat">
+                  <div className="indicator a"></div>
+                  <h5>Assigned</h5>
+                  <span id="assignedP">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator u"></div>
+                  <h5>Usused</h5>
+                  <span id="unusedP">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator r"></div>
+                  <h5>Retired</h5>
+                  <span id="retiredP">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator d"></div>
+                  <h5>Damaged</h5>
+                  <span id="damagedP">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator o"></div>
+                  <h5>Out of Sevice</h5>
+                  <span id="oosP">0</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="single-stat" id="monitors">
+              <div className="singleStat-total">
+                <i className="fal fa-desktop"></i>
+                <h4>Monitors</h4>
+                <h1 id="monitors-total">
+                  <div className="loading-mini"></div>
+                </h1>
+              </div>
+              <div className="single-stat-extra">
+                <div className="extra-stat">
+                  <div className="indicator a"></div>
+                  <h5>Assigned</h5>
+                  <span id="assignedM">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator u"></div>
+                  <h5>Unsused</h5>
+                  <span id="unusedM">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator r"></div>
+                  <h5>Retired</h5>
+                  <span id="retiredM">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator d"></div>
+                  <h5>Damaged</h5>
+                  <span id="damagedM">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator o"></div>
+                  <h5>Out of Sevice</h5>
+                  <span id="oosM">0</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="single-stat" id="networkdevices">
+              <div className="singleStat-total">
+                <i className="fal fa-router"></i>
+                <h4>Network Devices</h4>
+                <h1 id="networkdevices-total">
+                  <div className="loading-mini"></div>
+                </h1>
+              </div>
+              <div className="single-stat-extra">
+                <div className="extra-stat">
+                  <div className="indicator a"></div>
+                  <h5>Assigned</h5>
+                  <span id="assignedN">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator u"></div>
+                  <h5>Unsused</h5>
+                  <span id="unusedN">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator r"></div>
+                  <h5>Retired</h5>
+                  <span id="retiredN">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator d"></div>
+                  <h5>Damaged</h5>
+                  <span id="damagedN">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator o"></div>
+                  <h5>Out of Sevice</h5>
+                  <span id="oosN">0</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="single-stat" id="printers">
+              <div className="singleStat-total">
+                <i className="fal fa-print"></i>
+                <h4>Printers</h4>
+                <h1 id="printers-total">
+                  <div className="loading-mini"></div>
+                </h1>
+              </div>
+              <div className="single-stat-extra">
+                <div className="extra-stat">
+                  <div className="indicator a"></div>
+                  <h5>Assigned</h5>
+                  <span id="assignedPR">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator u"></div>
+                  <h5>Unsused</h5>
+                  <span id="unusedPR">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator r"></div>
+                  <h5>Retired</h5>
+                  <span id="retiredPR">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator d"></div>
+                  <h5>Damaged</h5>
+                  <span id="damagedPR">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator o"></div>
+                  <h5>Out of Sevice</h5>
+                  <span id="oosPR">0</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="single-stat" id="miscellaneous">
+              <div className="singleStat-total">
+                <i className="fal fa-stars"></i>
+                <h4>miscellaneous</h4>
+                <h1 id="misc-total">
+                  <div className="loading-mini"></div>
+                </h1>
+              </div>
+              <div className="single-stat-extra">
+                <div className="extra-stat">
+                  <div className="indicator a"></div>
+                  <h5>Assigned</h5>
+                  <span id="assignedMSC">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator u"></div>
+                  <h5>Unsused</h5>
+                  <span id="unusedMSC">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator r"></div>
+                  <h5>Retired</h5>
+                  <span id="retiredMSC">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator d"></div>
+                  <h5>Damaged</h5>
+                  <span id="damagedMSC">0</span>
+                </div>
+                <div className="extra-stat">
+                  <div className="indicator o"></div>
+                  <h5>Out of Sevice</h5>
+                  <span id="oosMSC">0</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="third-row">
+          <div className="left">
+            <h4>Recently Added Assets</h4>
+            <table className="recAdditions">
+              <thead className="thead">
+                <th>Category</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th></th>
+                <th>Added By</th>
+              </thead>
+              <tbody className="tbody" id="recent-table">
+                <div className="loading-mini-b" id="loading"></div>
+              </tbody>
+            </table>
+          </div>
+          <div className="right">
+            <h4>Asset Overview</h4>
+            <div className="overview">
+              <div className="chart">
+                <canvas id="myChart"></canvas>
+                <h1 id="totalStatusCount">
+                  <div className="loading-mini"></div>
+                </h1>
+              </div>
+              <div className="stats">
+                <div className="stat">
+                  <div className="indicator a"></div>
+                  <h4>Assigned Assets</h4>
+                  <span id="aStatusCount">0</span>
+                </div>
+                <div className="stat">
+                  <div className="indicator u"></div>
+                  <h4>Unused Assets</h4>
+                  <span id="uStatusCount">0</span>
+                </div>
+                <div className="stat">
+                  <div className="indicator r"></div>
+                  <h4>Retired Assets</h4>
+                  <span id="rStatusCount">0</span>
+                </div>
+                <div className="stat">
+                  <div className="indicator d"></div>
+                  <h4>Damaged Assets</h4>
+                  <span id="dStatusCount">0</span>
+                </div>
+                <div className="stat">
+                  <div className="indicator o"></div>
+                  <h4>Out of Service Assets</h4>
+                  <span id="oosStatusCount">0</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  );
+}
+
+export default Dashboard;
