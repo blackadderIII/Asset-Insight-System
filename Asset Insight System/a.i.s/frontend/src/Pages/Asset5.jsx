94987603055ef8d2784d7829132f88d2c7d90672
blackadderIII <skobarnes1@gmail.com>
2024-05-26 19:41:26 +0000

    .

---
+++ Asset Insight System/a.i.s/frontend/src/Pages/Asset5.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../css/Table.css'
+import Table from '../Components/Table';
+import {TitleComponent1} from '../Components/TitleComponent';
+
+function Asset6() {
+     
+  return (
+    <section class="main">
+        <TitleComponent1 title={'Printers'}/>
+
+        <div class="second-row">
+            <div class="buttons">
+                <div class="add-button" onclick="openModule()">
+                    <i class="fal fa-add"></i>
+                    <h5>Add</h5>
+                </div>
+                <div class="add-button" onclick="exportLaptops()">
+                    <i class="fal fa-file-export"></i>
+                    <h5>Export</h5>
+                </div>
+            </div>
+            <div class="field">
+                <input type="search" id="searchAsset" placeholder="Search..."/>
+                <i class="fal fa-search"></i>
+            </div>
+        </div>
+
+        <Table/>
+
+    </section>
+  )
+}
+
+export default Asset6
