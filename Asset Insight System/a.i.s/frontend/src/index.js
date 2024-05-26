8823b3be5a2b1dc2386fd627a9440b99775f3a10
blackadderIII <skobarnes1@gmail.com>
2024-05-26 19:42:02 +0000

    .

--- Asset Insight System/a.i.s/frontend/src/index.js
+++ Asset Insight System/a.i.s/frontend/src/index.js
@@ -15,20 +15,24 @@ import Dashboard from "./Pages/Dashboard.jsx";
 import Asset1 from "./Pages/Asset1.jsx";
 import Asset2 from "./Pages/Asset2.jsx";
 import Asset3 from "./Pages/Asset3.jsx";
-// import Asset4 from './Pages/Asset4.jsx';
-// import Asset5 from './Pages/Asset5.jsx';
-// import Asset6 from './Pages/Asset6.jsx';
+import Asset4 from './Pages/Asset4.jsx';
+import Asset5 from './Pages/Asset5.jsx';
+import Asset6 from './Pages/Asset6.jsx';
+import Users from './Pages/Users';
+import Settings from "./Pages/Settings.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromChildren([
     <Route path="/" element={<App />}>
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="" element={<Dashboard />} />
       <Route path="/Asset1" element={<Asset1 />} />
       <Route path="/Asset2" element={<Asset2 />} />
       <Route path="/Asset3" element={<Asset3 />} />
-      {/* <Route path='/Asset4' element={<Asset4/>}/>
+      <Route path='/Asset4' element={<Asset4/>}/>
       <Route path='/Asset5' element={<Asset5/>}/>
-      <Route path='/Asset6' element={<Asset6/>}/> */}
+      <Route path='/Asset6' element={<Asset6/>}/>
+      <Route path="/Users" element={<Users/>}/>
+      <Route path="/settings" element={<Settings/>}/>
     </Route>,
   ])
 );
