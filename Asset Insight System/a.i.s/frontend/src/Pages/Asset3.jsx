3ed32098d886152f39fad817c11528651cb76104
blackadderIII <skobarnes1@gmail.com>
2024-05-30 17:07:32 +0000

    .

--- Asset Insight System/a.i.s/frontend/src/Pages/Asset3.jsx
+++ Asset Insight System/a.i.s/frontend/src/Pages/Asset3.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import '../css/Table.css'
 import Table from '../Components/Table';
 import {TitleComponent1} from '../Components/TitleComponent';
+import { ModuleMNP } from './../Components/module';
 
 function Asset3() {
      
+    const [moduleActive,setModuleActive] = useState([null,"add-active","edit-active"]);
+
+    const openModule = (state) =>{
+        setModuleActive(
+          state  === moduleActive ? null : state
+        ) 
+    }
+
+    const closeModule = () => {
+        setModuleActive(null);
+      };
+
   return (
     <section class="main">
         <TitleComponent1 title={'Monitors'}/>
 
         <div class="second-row">
             <div class="buttons">
-                <div class="add-button" onclick="openModule()">
+                <div class="add-button" onClick={()=>openModule('add-active')}>
                     <i class="fal fa-add"></i>
                     <h5>Add</h5>
                 </div>
-                <div class="add-button" onclick="openModule()">
+                <div class="add-button" onClick={()=>openModule('edit-active')}>
                     <i class="fal fa-edit"></i>
                     <h5>Edit</h5>
                 </div>
@@ -31,6 +44,8 @@ function Asset3() {
         </div>
 
         <Table/>
+
+        <ModuleMNP asset={"Monitor"} modulestate={moduleActive} onClose={closeModule}/>
 
     </section>
   )
