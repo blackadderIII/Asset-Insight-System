125c807517cd23d2234cffc31606d783755a1c95
blackadderIII <skobarnes1@gmail.com>
2024-05-30 17:07:05 +0000

    .

--- Asset Insight System/a.i.s/frontend/src/Pages/Asset2.jsx
+++ Asset Insight System/a.i.s/frontend/src/Pages/Asset2.jsx
@@ -1,39 +1,61 @@
-import React from 'react';
-import '../css/Table.css'
-import Table from '../Components/Table';
-import {TitleComponent1} from '../Components/TitleComponent';
+import React, { useState } from "react";
+import "../css/Table.css";
+import Table from "../Components/Table";
+import { TitleComponent1 } from "../Components/TitleComponent";
+import { ModulePhone } from "../Components/module";
 
 function Asset2() {
-     
+  const [moduleActive, setModuleActive] = useState([
+    null,
+    "add-active",
+    "edit-active",
+  ]);
+
+  const openModule = (state) => {
+    setModuleActive(state === moduleActive ? null : state);
+  };
+
+  const closeModule = () => {
+    setModuleActive(null);
+  };
+
   return (
-    <section class="main">
-        <TitleComponent1 title={'Phones'}/>
+    <>
+      <section class="main">
+        <TitleComponent1 title={"Phones"} />
 
         <div class="second-row">
-            <div class="buttons">
-                <div class="add-button" onclick="openModule()">
-                    <i class="fal fa-add"></i>
-                    <h5>Add</h5>
-                </div>
-                <div class="add-button" onclick="openModule()">
-                    <i class="fal fa-edit"></i>
-                    <h5>Edit</h5>
-                </div>
-                <div class="add-button" onclick="exportLaptops()">
-                    <i class="fal fa-file-export"></i>
-                    <h5>Export</h5>
-                </div>
+          <div class="buttons">
+            <div class="add-button" onClick={
+                ()=>openModule("add-active")}>
+              <i class="fal fa-add"></i>
+              <h5>Add</h5>
             </div>
-            <div class="field">
-                <input type="search" id="searchAsset" placeholder="Search..."/>
-                <i class="fal fa-search"></i>
+            <div class="add-button" onClick={()=>openModule("edit-active")}>
+              <i class="fal fa-edit"></i>
+              <h5>Edit</h5>
             </div>
+            <div class="add-button" onclick="exportLaptops()">
+              <i class="fal fa-file-export"></i>
+              <h5>Export</h5>
+            </div>
+          </div>
+          <div class="field">
+            <input type="search" id="searchAsset" placeholder="Search..." />
+            <i class="fal fa-search"></i>
+          </div>
         </div>
 
-        <Table/>
-
-    </section>
-  )
+        <Table />
+        
+      </section>
+      <ModulePhone
+        asset={"Phone"}
+        modulestate={moduleActive}
+        onClose={closeModule}
+      />
+    </>
+  );
 }
 
-export default Asset2
+export default Asset2;
