06b379e3086db1f5b10ea588a6a9420950f87432
blackadderIII <skobarnes1@gmail.com>
2024-05-31 11:15:25 +0000

    .

--- Asset Insight System/a.i.s/frontend/src/Components/Table.jsx
+++ Asset Insight System/a.i.s/frontend/src/Components/Table.jsx
@@ -1,8 +1,9 @@
-import React,{createRef} from "react";
+import React, { createRef } from "react";
 import "../css/Table.css";
+import emptyPng from "../Assets/icons/empty.png";
 
-export default function Table({ laptops,loading}) {
-    const loadingRef = createRef();
+export default function Table({ laptops, loading }) {
+  const loadingRef = createRef();
   return (
     <div class="third-row">
       <table id="laptops">
@@ -20,59 +21,63 @@ export default function Table({ laptops,loading}) {
         </thead>
 
         <tbody id="main-table">
-        {loading ? (<div className="loading" ref={loadingRef}></div>):(
+          {loading ? (
+            <div className="loading" ref={loadingRef}></div>
+          ) : laptops.length === 0 ? (
+            <div>
+              <img src={emptyPng} alt="No laptops" />
+            </div>
+          ) : (
             laptops.map((laptop) => (
-          <tr key={laptop.sn}>
-            <td>{laptop.sn}</td>
-            <td>{laptop.brand}</td>
-            <td>{laptop.model}</td>
-            <td>
-              {laptop.status === "Assigned" ? (
-                <div className="status a">Assigned</div>
-              ) : laptop.status === "Unused" ? (
-                <div className="status u">Unused</div>
-              ) : laptop.status === "Damaged" ? (
-                <div className="status d">Damaged</div>
-              ) : laptop.status === "Out of Service" ? (
-                <div className="status oos">Out of Service</div>
-              ) : (
-                <div className="status r">Retired</div>
-              )}
-            </td>
-            <td>
-              {laptop.username ? (
-                <div className="user">
-                  <h4>{laptop.username}</h4>
-                  <a href="#">{laptop.useremail}</a>
-                </div>
-              ) : (
-                <div className="user">
-                  <h4>N/A</h4>
-                  <a href="#">N/A</a>
-                </div>
-              )}
-            </td>
-            <td>
-              {new Date(laptop.dateadded).toLocaleDateString()} |{" "}
-              {new Date(laptop.dateadded).toLocaleTimeString()}
-            </td>
-            <td>
-              <button id="assign">Assign</button>
-            </td>
-            <td>
-              <button id="revoke">Revoke</button>
-            </td>
-            <td>
-              <i className="far fa-edit" id="edit"></i>
-            </td>
-            <td>
-              <i className="far fa-trash-alt" id="delete"></i>
-            </td>
-          </tr>
-          ))
-        )}
-
-          
+              <tr key={laptop.sn}>
+                <td>{laptop.sn}</td>
+                <td>{laptop.brand}</td>
+                <td>{laptop.model}</td>
+                <td>
+                  {laptop.status === "Assigned" ? (
+                    <div className="status a">Assigned</div>
+                  ) : laptop.status === "Unused" ? (
+                    <div className="status u">Unused</div>
+                  ) : laptop.status === "Damaged" ? (
+                    <div className="status d">Damaged</div>
+                  ) : laptop.status === "Out of Service" ? (
+                    <div className="status oos">Out of Service</div>
+                  ) : (
+                    <div className="status r">Retired</div>
+                  )}
+                </td>
+                <td>
+                  {laptop.username ? (
+                    <div className="user">
+                      <h4>{laptop.username}</h4>
+                      <a href="#">{laptop.useremail}</a>
+                    </div>
+                  ) : (
+                    <div className="user">
+                      <h4>N/A</h4>
+                      <a href="#">N/A</a>
+                    </div>
+                  )}
+                </td>
+                <td>
+                  {new Date(laptop.dateadded).toLocaleDateString()} |{" "}
+                  {new Date(laptop.dateadded).toLocaleTimeString()}
+                </td>
+                <td>
+                  <button id="assign">Assign</button>
+                </td>
+                <td>
+                  <button id="revoke">Revoke</button>
+                </td>
+                <td>
+                  <i className="far fa-edit" id="edit"></i>
+                </td>
+                <td>
+                  <i className="far fa-trash-alt" id="delete"></i>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
 
         <tbody class="inactive" id="search-table"></tbody>
