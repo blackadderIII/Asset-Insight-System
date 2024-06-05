e016d3c769e1bc2cbfd88f2f77fa2610d9ac9b27
blackadderIII <skobarnes1@gmail.com>
2024-06-05 20:50:40 +0000

    .

--- Asset Insight System/a.i.s/frontend/src/Pages/Asset5.jsx
+++ Asset Insight System/a.i.s/frontend/src/Pages/Asset5.jsx
@@ -54,11 +54,11 @@ useEffect(() => {
 
         <div class="second-row">
             <div class="buttons">
-                <div class="add-button" onclick="openModule()">
+            <div class="add-button" onClick={()=>openModule('add-active')}>
                     <i class="fal fa-add"></i>
                     <h5>Add</h5>
                 </div>
-                <div class="add-button" onclick="openModule()">
+                <div class="add-button" onClick={()=>openModule('edit-active')}>
                     <i class="fal fa-edit"></i>
                     <h5>Edit</h5>
                 </div>
