52a2f6df58e7b067fe6f2d69d4f978d8d7bfcd19
blackadderIII <skobarnes1@gmail.com>
2024-06-09 19:30:23 +0000

    .

--- Asset Insight System/a.i.s/frontend/src/Pages/Asset6.jsx
+++ Asset Insight System/a.i.s/frontend/src/Pages/Asset6.jsx
@@ -1,52 +1,60 @@
-import React from 'react';
-import '../css/Table.css'
-import Table from '../Components/Table';
-import {TitleComponent1} from '../Components/TitleComponent';
+import React from "react";
+import "../css/Table.css";
+import Table from "../Components/Table";
+import { TitleComponent1 } from "../Components/TitleComponent";
 
 function Asset5() {
-    const [moduleActive,setModuleActive] = useState([null,"add-active","edit-active"]);
-    
-    const openModule = (state) =>{
-        setModuleActive(
-          state  === moduleActive ? null : state
-        ) 
-    }
-    const closeModule = () => {
-        setModuleActive(null);
-      };
-      const [moduleEdit,setModuleEdit] = useState([]);
+  const [moduleActive, setModuleActive] = useState([
+    null,
+    "add-active",
+    "edit-active",
+  ]);
 
-      
+  const openModule = (state) => {
+    setModuleActive(state === moduleActive ? null : state);
+  };
+  const closeModule = () => {
+    setModuleActive(null);
+  };
+  const [moduleEdit, setModuleEdit] = useState([]);
+
+  const [showEditModule, setShowEditModule] = useState(false);
+  const closeEditModule = () => {
+    setShowEditModule(null);
+  };
+
+  const handleEditClick = (monitor) => {
+    setShowEditModule(true);
+    setModuleEdit(monitor);
+  };
   return (
     <section class="main">
-        <TitleComponent1 title={'Miscellaneous Items'}/>
+      <TitleComponent1 title={"Miscellaneous Items"} />
 
-        <div class="second-row">
-            <div class="buttons">
-            <div class="add-button" onClick={()=>openModule('add-active')}>
-                    <i class="fal fa-add"></i>
-                    <h5>Add</h5>
-                </div>
-                <div class="add-button" onClick={()=>openModule('edit-active')}>
-                    <i class="fal fa-edit"></i>
-                    <h5>Edit</h5>
-                </div>
-                <div class="add-button" onclick="exportLaptops()">
-                    <i class="fal fa-file-export"></i>
-                    <h5>Export</h5>
-                </div>
-            </div>
-            <div class="field">
-                <input type="search" id="searchAsset" placeholder="Search..."/>
-                <i class="fal fa-search"></i>
-            </div>
+      <div class="second-row">
+        <div class="buttons">
+          <div class="add-button" onClick={() => openModule("add-active")}>
+            <i class="fal fa-add"></i>
+            <h5>Add</h5>
+          </div>
+          <div class="add-button" onClick={() => openModule("edit-active")}>
+            <i class="fal fa-edit"></i>
+            <h5>Edit</h5>
+          </div>
+          <div class="add-button" onclick="exportLaptops()">
+            <i class="fal fa-file-export"></i>
+            <h5>Export</h5>
+          </div>
         </div>
+        <div class="field">
+          <input type="search" id="searchAsset" placeholder="Search..." />
+          <i class="fal fa-search"></i>
+        </div>
+      </div>
 
-
-        <Table/>
-
+      <Table />
     </section>
-  )
+  );
 }
 
-export default Asset5
+export default Asset5;
