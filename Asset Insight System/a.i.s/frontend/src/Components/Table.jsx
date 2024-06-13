c27f0ef988eeffd16ec827412e5d0b0e18f160c9
blackadderIII <skobarnes1@gmail.com>
2024-06-13 22:05:53 +0000

    .

--- Asset Insight System/a.i.s/frontend/src/Components/Table.jsx
+++ Asset Insight System/a.i.s/frontend/src/Components/Table.jsx
@@ -89,32 +89,3 @@ export default function Table({ asset,loading,onEdit}) {
  );
 }
 
-// export function TableSupplier() {
-//   return (
-//     <div class="third-row">
-//             <table id="laptops">
-//                 <thead>
-//                     <th>SN</th>
-//                     <th>Brand</th>
-//                     <th>Model</th>
-//                     <th></th>
-//                     <th>User</th>
-//                     <th>Date Added</th>
-//                     <th></th>
-//                     <th></th>
-//                     <th></th>
-//                     <th></th>
-//                 </thead>
-
-//                 <tbody id="main-table">
-
-//                     <div class="loading" id="loading"></div>
-
-//                 </tbody>
-
-//                 <tbody class="inactive" id="search-table">
-//                 </tbody>
-//             </table>
-//         </div>
-//   )
-// }
