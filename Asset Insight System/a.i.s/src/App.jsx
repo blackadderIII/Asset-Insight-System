c4afcfe6a28acffa817fa0dcaf099a7f1b95c939
blackadderIII <skobarnes1@gmail.com>
2024-06-23 11:44:00 +0000

    .

---
+++ Asset Insight System/a.i.s/src/App.jsx
@@ -0,0 +1,45 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import "./css/all.css";
+import Navbar from "./Components/Navbar";
+import Toasts from "./Components/Toasts";
+import Titlebar from "./Components/Titlebar";
+import { useState, useEffect } from "react";
+import { ThemeContext } from "./utils/themeContext";
+
+function App() {
+  // Theme Toggle Function
+
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem("theme");
+    return storedTheme ? storedTheme : "light";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    applyTheme(theme);
+  }, [theme]);
+
+  const applyTheme = (theme) => {
+    document.body.classList.remove("dark", "light");
+    document.body.classList.add(theme);
+  };
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={toggleTheme}>
+      <main>
+        <Titlebar />
+        <Navbar toggleTheme={() => toggleTheme()}/>
+        <Toasts />
+        <section className="render-space">
+          <Outlet />
+        </section>
+      </main>
+    </ThemeContext.Provider>
+  );
+}
+export default App;
---
+++ Asset Insight System/a.i.s/src/Pages/Users.jsx
@@ -0,0 +1,69 @@
+import React,{useState,useEffect} from 'react'
+import { TitleComponent1 } from '../Components/TitleComponent';
+import {UserTable}from '../Components/Table';
+
+export default function Users() {
+    const [moduleActive, setModuleActive] = useState([
+        null,
+        "add-active",
+      ]);
+      const openModule = (state) => {
+        setModuleActive(state === moduleActive ? null : state);
+      };
+      const closeModule = () => {
+    setModuleActive(null);
+  };
+
+  const [moduleEdit, setModuleEdit] = useState([]);
+  const [showEditModule, setShowEditModule] = useState(false);
+  const handleEditClick = (monitor) => {
+    setShowEditModule(true);
+    setModuleEdit(monitor);
+  };
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // get Users
+useEffect(() => {
+  async function getUsers() {
+    try {
+      const response = await fetch(`http://localhost:3300/getUsers`);
+      const data = await response.json();
+      setUsers(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  }
+  getUsers();
+}, []);
+
+  return (
+    <section class="main">
+    <TitleComponent1 title={'Users'}/>
+
+    <div class="second-row">
+        <div class="buttons">
+        <div class="add-button" onClick={() => openModule("add-active")}>
+            <i class="fal fa-add"></i>
+            <h5>Add</h5>
+          </div>
+            <div class="add-button" onclick="exportLaptops()">
+                <i class="fal fa-file-export"></i>
+                <h5>Export</h5>
+            </div>
+        </div>
+        <div class="field">
+            <input type="search" id="searchAsset" placeholder="Search..."/>
+            <i class="fal fa-search"></i>
+        </div>
+    </div>
+
+    <UserTable data={users} loading={loading} onEdit={handleEditClick}/>
+
+</section>
+  )
+}
