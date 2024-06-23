bc6b6f87a03a83f856477ae423999c08599539ed
blackadderIII <skobarnes1@gmail.com>
2024-06-23 11:42:56 +0000

    .

---
+++ Asset Insight System/a.i.s/src/Components/Toasts.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "../css/toast.css";
+
+function Toasts() {
+  const successToast = document.getElementById("toast-success");
+  const successMessage = document.getElementById("toast-success-message");
+  const warnToast = document.getElementById("toast-warn");
+  const warnMessage = document.getElementById("toast-warn-message");
+  const errorToast = document.getElementById("toast-error");
+  const errorMessage = document.getElementById("toast-error-message");
+
+  const successT = (message) => {
+    successToast.classList.add("active");
+    successMessage.innerHTML = message;
+    setTimeout(() => successToast.classList.remove("active"), 4000);
+    setTimeout(() => (successMessage.innerHTML = ""), 4100);
+  };
+
+  const successTPermanent = (message) => {
+    successToast.classList.add("active");
+    successMessage.innerHTML = message;
+  };
+
+  const warnT = (message) => {
+    warnToast.classList.add("active");
+    warnMessage.innerHTML = message;
+    setTimeout(() => warnToast.classList.remove("active"), 4000);
+    setTimeout(() => (warnMessage.innerHTML = ""), 4100);
+  };
+
+  const warnTPermanent = (message) => {
+    warnToast.classList.add("active");
+    warnMessage.innerHTML = message;
+  };
+
+  const errorT = (message) => {
+    errorToast.classList.add("active");
+    errorMessage.innerHTML = message;
+    setTimeout(() => errorToast.classList.remove("active"), 4000);
+    setTimeout(() => (errorMessage.innerHTML = ""), 4100);
+  };
+
+  const errorTPermanent = (message) => {
+    errorToast.classList.add("active");
+    errorMessage.innerHTML = message;
+  };
+
+  return (
+    <div>
+      <div className="toast success" id="toast-success">
+        <i className="fas fa-check-circle"></i>
+        <p id="toast-success-message">Success adding laptop</p>
+      </div>
+
+      <div className="toast warn" id="toast-warn">
+        <i className="fas fa-warning"></i>
+        <p id="toast-warn-message">Failed to reach servers</p>
+      </div>
+
+      <div className="toast error" id="toast-error">
+        <i className="fas fa-times-circle"></i>
+        <p id="toast-error-message">Success adding laptop</p>
+      </div>
+    </div>
+  );
+}
+
+export default Toasts;
