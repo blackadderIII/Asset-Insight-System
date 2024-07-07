a1d710b991bf9beca831c3e37d4137c329253d48
blackadderIII <skobarnes1@gmail.com>
2024-07-07 20:48:18 +0000

    .

--- Asset Insight System/a.i.s/src/Components/module.jsx
+++ Asset Insight System/a.i.s/src/Components/module.jsx
@@ -134,7 +134,6 @@ export function ModuleLaptop({
 
       setLoading(false);
       handleClose();
-      setComment("");
       successT("Laptop added successfully!");
     } catch (error) {
       errorT("An error occured. please try again later.");
