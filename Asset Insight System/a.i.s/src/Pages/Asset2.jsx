a25d99e7e70844a94dfc61b592cd3e4097090fe9
blackadderIII <skobarnes1@gmail.com>
2024-08-04 17:38:02 +0000

    .

--- Asset Insight System/a.i.s/src/Pages/Asset2.jsx
+++ Asset Insight System/a.i.s/src/Pages/Asset2.jsx
@@ -125,6 +125,7 @@ const handleDeleteClick = (laptop) => {
         closeEditModule={closeEditModule}
         showAssignModule={showAssignModule}
         userList={userList}
+        
       />
     </>
   );
